df3f446c48e9df03ebb08bd2fc04cb41690beed3
Sergey Aldoukhov <saldoukhov@gmail.com>
2014-09-21 13:11:17 -0700

    LinqKit framework attached, new strings playgrouns

--- Playgrounds/Strings.playground/section-1.swift
+++ Playgrounds/Strings.playground/section-1.swift
@@ -1,5 +1,16 @@
 import UIKit
+import LinqKit
 
-var str = "Hello, playground"
 
 ",".join(["a", "b", "c"])
+
+let flags = "🇺🇸US🇷🇺RF"
+let count1 = countElements(flags)
+let count2 = flags.utf16Count
+let count3 = (flags as NSString).length
+let count4 = flags.lengthOfBytesUsingEncoding(NSUTF16StringEncoding)
+let count5 = flags.lengthOfBytesUsingEncoding(NSUTF8StringEncoding)
+
+// LinqKit
+
+let l = flags.length
