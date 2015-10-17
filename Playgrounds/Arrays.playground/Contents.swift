a6870322bcbf80c5c7649438551075c034b44022
Sergey Aldoukhov <saldoukhov@gmail.com>
2015-10-16 17:20:44 -0700

    Arrays converted

--- Playgrounds/Arrays.playground/Contents.swift
+++ Playgrounds/Arrays.playground/Contents.swift
@@ -1,16 +1,16 @@
+//: Playground - noun: a place where people can play
+
 import UIKit
 
-let arr = ["zz", "ff", "aa"]
+var arr = ["zz", "ff", "aa"]
 
-let sarr1 =
-arr.sorted( { s1, s2 in return s1 < s2 } )
+let sarr1 = arr.sort( { s1, s2 in return s1 < s2 } )
 
-let sarr2 =
-sorted(arr, { s1, s2 in return s1 < s2 } )
+arr.sortInPlace({ s1, s2 in return s1 < s2 })
 
-let sarr3 = arr.sorted( { $0 < $1 } )
+let sarr3 = arr.sort( { $0 < $1 } )
 
-let sarr4 = arr.sorted( < )
+let sarr4 = arr.sort( < )
 
 let nsArr = NSArray(objects: "zz", "ff", "aa")
 let nsArr1 = NSArray(array: arr)
@@ -20,59 +20,63 @@ let arrFromNS = Array(nsArr)
 var marr = ["zz", "ff", "aa"]
 
 marr.sort( { s1, s2 in return s1 < s2 } )
+marr
 
 let narr = [1, 2, 3, 4]
 
-[].join([[1, 2], [3, 4], [5, 6]])
+let flat = [[1, 2], [3, 4], [5, 6]].flatMap( { $0 } )
+flat
 
-[100].join([[1, 2], [3, 4], [5, 6]])
+["a","b","c"].joinWithSeparator(",")
 
-let narrFiltered =
-narr.filter( { $0 < 3 } )
+let narrFiltered = narr.filter( { $0 < 3 } )
 
-let narrMapped =
-narr.map( { String($0) } )
+let narrMapped = narr.map(String.init)
 
 let n = 1
+
 narr[0...n]
+
 narr[narr.startIndex..<narr.endIndex]
-narr[0..<advance(narr.startIndex, narr.count)]
-Slice(narr)
+
+narr[0..<narr.startIndex.advancedBy(narr.count)]
+
+let r = Range(start: narr[0], end: narr[2])
+let slice = Slice(base: narr, bounds: r)
+Array(slice)
+
 
 let n2 = narr.startIndex.advancedBy(2)
+
 narr.startIndex
 narr.endIndex
 narr.count
-let n3 = advance(n2, 1)
+
 narr[n2...n2+1] == [3, 4]
 
 Array(count: 10, repeatedValue: 1)
 
-let zipped = Array(Zip2(["a", "b", "c"], [1, 2]))
+let zipped = Array(zip(["a", "b", "c"], [1, 2]))
 
 let firstNum = narr.first!
 
 let sumBelow4 = narr
     .filter( { $0 < 4 } )
-    .reduce(0, +)
+    .reduce(0, combine: +)
+sumBelow4
 
-Array("abcd")
-[Character]("abcd")
-let chars1 =
-Array(map("abcd", { $0 }))
-let chars2 =
-Array(lazy("abcd").map({ $0 }))
+[Character]("abcd".characters)
 
 class Numbers : SequenceType {
-    let max: Int = 10
+    var max: Int = 10
     
     init(max: Int) {
         self.max = max
     }
     
-    func generate() -> GeneratorOf<Int> {
+    func generate() -> AnyGenerator<Int> {
         var i = 0
-        return GeneratorOf<Int> {
+        return anyGenerator() {
             if i >= self.max { return .None }
             return .Some(i++)
         }
@@ -80,3 +84,4 @@ class Numbers : SequenceType {
 }
 
 let nums = Numbers(max: 5)
+Array(nums)
